9800d32abfacfd4432793fe67776f559ab6df8e6
Dario Voll <dariovoll@gmail.com>
2023-11-20 21:07:45 +0100

    Fix footer spacing

--- themes/eee-theme/patterns/footer-default.php
+++ themes/eee-theme/patterns/footer-default.php
@@ -9,8 +9,8 @@
 <!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"secondary","textColor":"white","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-white-color has-secondary-background-color has-text-color has-background has-link-color"
 	style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
+	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide">
 		<!-- wp:site-title {"level":0} /-->
 
 		<!-- wp:paragraph -->
