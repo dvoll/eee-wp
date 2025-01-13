271b926a0e514c5dc37821818fd12de9c89be12b
Dario Voll <dariovoll@gmail.com>
2025-01-13 22:12:35 +0100

    Update plugin setup file

--- wp-plugin-setup.sh
+++ wp-plugin-setup.sh
@@ -1,2 +1,4 @@
-# docker compose run --rm wp-cli plugin install layout-grid --activate
 # docker compose run --rm wp-cli plugin install wordpress-seo --activate
+docker compose run --rm wp-cli plugin activate eee23-blocks
+# docker compose run --rm wp-cli language plugin install form-block de_DE
+docker compose run --rm wp-cli install hkdev-maintenance-mode --activate
